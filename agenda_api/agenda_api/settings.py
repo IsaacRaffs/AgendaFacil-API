4e2226279cb71aab5c77c7d3f1130e0a262ad14b
unknown <isaacrafaelmoraes.dev@gmail.com>
2024-04-02 21:46:59 -0300

    teste5

--- agenda_api/agenda_api/settings.py
+++ agenda_api/agenda_api/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['agendafacil-api.onrender.com']
 #
-CORS_ALLOWED_ORIGINS = ['http://127.0.0.1:5500']
+CORS_ALLOWED_ORIGINS = ['https://agendafacil-api.vercel.app']
 
 # Application definition
 
