3d29cc0dcc23e556fd124d5e10fb30d7de1fa868
unknown <isaacrafaelmoraes.dev@gmail.com>
2024-04-02 21:28:26 -0300

    teste

--- agenda_api/agenda_api/settings.py
+++ agenda_api/agenda_api/settings.py
@@ -80,11 +80,14 @@ WSGI_APPLICATION = 'agenda_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.vreelumnxpaqfsaqbnkc',
+        'PASSWORD': '#isaacbruno#',
+        'HOST': 'aws-0-us-west-1.pooler.supabase.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
