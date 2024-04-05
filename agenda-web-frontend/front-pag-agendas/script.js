b2f8a3c2e7783693ed2826e62b5cfc844c8b8d0d
unknown <isaacrafaelmoraes.dev@gmail.com>
2024-04-04 22:22:44 -0300

    modificar

--- agenda-web-frontend/front-pag-agendas/script.js
+++ agenda-web-frontend/front-pag-agendas/script.js
@@ -20,7 +20,7 @@ async function iniciarModificarAgenda(id) {
     if (response.status === 200) {
 
         const agenda = await response.json()
-
+        console.log(agenda.nome, agenda.email, agenda.data, agenda.n_cll)
         const nome = agenda.nome 
         const email = agenda.email
         const n_cll = agenda.n_cll
